29da7c87c3ecfc65f76656da8db03618292ddaec
tevin davis <aod.td1105@gmail.com>
2020-03-17 21:47:19 -0400

    Setup Protected Routes for app

--- auth-friends/src/App.js
+++ auth-friends/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Link, Route, withRouter } from 'react-router-dom';
+import { getToken } from './utils/api';
+import ProtectedRoute from './components/ProtectedRoute';
+import Account from './components/Account';
+import FriendsList from './components/FriendsList';
+import FriendsForm from './components/FriendsForm';
+import Login from './components/Login';
+import Logout from './components/Logout';
 import './App.css';
 
 function App() {
+  const loggedOn = getToken();
+
   return (
     <div className="App">
-      <h1>Friends</h1>
+      {!loggedOn && <Link to='/login'>Login</Link>}
+      <Route exact path="/login" component={Login} />
+      <ProtectedRoute exact path="/account" component={Account} />
+      <ProtectedRoute exact path="/account/friends" component={FriendsList} />
+      <ProtectedRoute exact path="/account/friends/add-friends" component={FriendsForm} />
+      <ProtectedRoute exact path="/account/logout" component={Logout} />
     </div>
   );
 }
---
+++ auth-friends/src/components/Account.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import '../App.css';
+
+
+function Account() {
+    
+    
+    return (
+        <>
+            <Link className="links" to="/account/friends">Friends</Link>
+            <Link className="links" to="/account/friends/add-friends">Add Friends</Link>
+            <Link className="links" to="/account/logout">Logout</Link>
+        </>
+    )
+}
+
+export default withRouter(Account)
